69004c80e77bec2668f57bab3efb438ee9016706
pigeonIlesh <ilyanikulin0820@gmail.com>
2025-12-15 21:16:10 +0300

    доделал

---
+++ pr9.10/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr9._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 3; i++)
+            {
+                char low = Convert.ToChar(r.Next(97, 123));
+                sb.Append(low);
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                char zif = Convert.ToChar(r.Next(48, 58));
+                sb.Append(zif);
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                char high = Convert.ToChar(r.Next(65, 91));
+                sb.Append(high);
+            }
+            string password = sb.ToString();
+            Console.WriteLine(password);
+            char[] mas = password.ToCharArray();
+            for (int i = mas.Length - 1; i > 0; i--)
+            {
+                int j = r.Next(i + 1);  
+                char t = mas[i];
+                mas[i] = mas[j];
+                mas[j] = t;
+            }
+            StringBuilder sb1 = new StringBuilder();
+            foreach (char c in mas)
+            {
+                sb1.Append(c);
+            }
+            Console.WriteLine(sb1.ToString());
+        }
+    }
+}
---
+++ pr9.9/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace pr9._9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Введите слово на английском");
+            string word = Console.ReadLine();
+            string slovo = word.Trim().ToLower();
+            StringBuilder sb = new StringBuilder();
+            foreach (char s in slovo)
+            {
+                if (s >= 'a' && s <= 'z')
+                {
+                    char c = Convert.ToChar(s + 3);
+                    if (c > 'z')
+                    {
+                        c = Convert.ToChar(c - 26);
+                    }
+                    sb.Append(c);
+                }
+                else
+                {
+                    sb.Append(s);
+                }
+            }
+            string shifr = sb.ToString();
+            Console.WriteLine($"Зашифрованное слово: {shifr}");
+        }
+    }
+}
